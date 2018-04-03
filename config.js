8345fca284e201eb5a44560785d2437e244fac0f
DaveRingelnatz <37311128+DaveRingelnatz@users.noreply.github.com>
2018-04-03 23:22:00 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,12 +2,12 @@ let path = require('path')
 global.rootPath = path.normalize(path.join(__dirname, '..', '..'))
 
 module.exports = {
-	
-	// field_exporter config	
-	field_node_public_id: '',
-	field_nodes_multiple_ids: [],
 
-  // address and port where the exporter will be bound
-  bind_address: '127.0.0.1',
-  bind_port: 9337,
+    // field_exporter config	
+    field_node_public_id: '',
+    field_nodes_multiple_ids: [],
+
+    // address and port where the exporter will be bound
+    bind_address: '127.0.0.1',
+    bind_port: 9337,
 }
