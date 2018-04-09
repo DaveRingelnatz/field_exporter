a2fb7fee114c3809ffa69a73147c9aff6a15eaa8
DaveRingelnatz <37311128+DaveRingelnatz@users.noreply.github.com>
2018-04-09 10:08:45 +0200

    Update and rename config.js to config-template.js

--- config-template.js
+++ config-template.js
@@ -3,8 +3,11 @@ global.rootPath = path.normalize(path.join(__dirname, '..', '..'))
 
 module.exports = {
 
-    // field_exporter config	
+    // field_exporter config
+    // ALWAYS USE YOUR publicID, NOT YOUR FIELD NODE's NAME
+    // example entry: field_node_public_id: 'yourpublicidhere',
     field_node_public_id: '',
+    // example config: "field_nodes_public_ids_array: ['publicId1','publicId2'],"
     field_nodes_public_ids_array: [],
 
     // address and port where the exporter will be bound
